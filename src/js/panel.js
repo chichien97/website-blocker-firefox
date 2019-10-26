86d7a32ce05de67d1cd58bfcfdb1ca4c1bbe6b27
Ang Shi Guang <41531133+fr33dom97@users.noreply.github.com>
2019-10-26 11:39:19 +0800

    Add files via upload

--- src/js/panel.js
+++ src/js/panel.js
@@ -3,6 +3,37 @@
     var browser = browser || chrome;
     var bgpage = browser.extension.getBackgroundPage();
 
+    function setPassword(){
+        var passInput = document.getElementById("sPassword");
+        var btnSet = document.getElementById("btn-Set-Password");
+        addClass(btnSet, "disappear");
+        removeClass(passInput, "disappear");
+    }
+
+    function verifyPassword(){
+        var btnUnlock = document.getElementById("btn-Unlock");
+        var inputVPass = document.getElementById("vPassword");
+        addClass(btnUnlock, "disappear");
+        removeClass(inputVPass, "disappear");
+    }
+
+    function checkPassCode(){
+        chrome.storage.local.get("unlockPassCode", function(items){
+            if(items.unlockPassCode == undefined){
+                var btnUnlock = document.getElementById("btn-Unlock");
+                var btnSet = document.getElementById("btn-Set-Password");
+                addClass(btnUnlock, "disappear");
+                removeClass(btnSet, "disappear");
+            }
+            else if(items.unlockPassCode != undefined){
+                var btnUnlock = document.getElementById("btn-Unlock");
+                var btnSet = document.getElementById("btn-Set-Password");
+                addClass(btnSet, "disappear");
+                removeClass(btnUnlock, "disappear");
+            }
+        });
+    }
+    
     function toggleAddIcon(isWhitelistMode) {
         browser.tabs.query({
             active: true,
@@ -39,6 +70,9 @@
                 blacklistSwitch.checked = true;
                 whitelistSwitch.checked = false;
             }
+            // if(statusSwitch.checked = true){
+                checkPassCode();
+            // }
             toggleAddIcon(isWhitelistMode);
         }
         else
@@ -125,10 +159,26 @@
         }
         else if (t.id == "status-switch") {
             var value = t.checked;
-            bgpage.setIsEnabled(value);
-            browser.storage.local.set({
-                isEnabled: value
-            }, function() {});
+            var input = document.getElementById("btn-Set-Password");
+            if(value){
+                bgpage.setIsEnabled(value);
+                browser.storage.local.set({
+                    isEnabled: value
+                }, function() {});
+            }
+            else if(!value){
+                if(input.id == "btn-Set-Password" && hasClass(input, "disappear")){
+                    // this.alert(value);
+                    t.checked = true;
+                }
+                else{
+                    // this.alert(value);
+                    bgpage.setIsEnabled(value);
+                    browser.storage.local.set({
+                        isEnabled: value
+                    }, function() {});
+                }
+            } 
         }
         else if ((t.id == "blacklist-switch" && bgpage.getIsWhitelistMode()) || (t.id == "whitelist-switch" && !bgpage.getIsWhitelistMode())) {
             var isWhitelistMode = t.id == "blacklist-switch" && t.checked ? false : true;
@@ -139,12 +189,93 @@
             toggleAddIcon(isWhitelistMode);
             setText("main_add_blacklist_tooltip", isWhitelistMode ? browser.i18n.getMessage("main_add_whitelist_tooltip") : browser.i18n.getMessage("main_add_blacklist_tooltip"));
         }
+        else if(t.id == "btn-Set-Password"){
+            setPassword();
+        }
+        else if(t.id == "btn-Unlock"){
+            chrome.storage.local.get("unlockPassCode", function(items){
+                // alert(items.unlockPassCode);
+                verifyPassword(items.unlockPassCode);
+            })
+            // unlockPassCode = bgpage.getUnlockPassCode();
+            // alert(t.id+"."+unlockPassCode);
+        }
     }, false);
 
     window.addEventListener("contextmenu", function(event) {
         event.preventDefault();
         return false;
     }, true);
+
+    window.addEventListener("keypress", function(event){
+        var tgt = event.target;
+        // alert(tgt.id);
+        if(tgt.id == "sPassword"){
+            var inputPwd = document.getElementById("sPassword");
+            var btnUnlock = document.getElementById("btn-Unlock");
+            var statusPwd = document.getElementById("set-Pwdstatus");
+            var statusSwitch = document.getElementById("status-switch");
+            var keyCode = event.keyCode || event.which;
+            if (keyCode === 13) {
+                addClass(inputPwd, "disappear");
+                removeClass(statusPwd, "disappear");
+                setTimeout(function(){
+                    addClass(statusPwd, "disappear");
+                    removeClass(btnUnlock, "disappear");
+                    statusSwitch.checked = true;
+                    bgpage.setIsEnabled(true);
+                    bgpage.setUnlockPassCode(inputPwd.value);
+                    browser.storage.local.set({
+                        isEnabled: true,
+                    }, function() {});
+                    // chrome.storage.local.get("unlockPassCode", function(items){
+                    //     alert(items.unlockPassCode);
+                    // })
+                }, 500);
+                chrome.storage.local.set({
+                    unlockPassCode: inputPwd.value
+                })
+            }
+        }
+        else if (tgt.id == "vPassword"){
+            var inputPwd = document.getElementById("vPassword");
+            var btnSet = document.getElementById("btn-Set-Password");
+            var statusPwd = document.getElementById("set-Pwdstatus");
+            var statusSwitch = document.getElementById("status-switch");
+            var keyCode = event.keyCode || event.which;
+            chrome.storage.local.get("unlockPassCode", function(items){
+                var passCode = items.unlockPassCode;
+                if (keyCode === 13) {
+                    if(inputPwd.value == passCode){
+                        addClass(inputPwd, "disappear");
+                        removeClass(statusPwd, "disappear");
+                        statusPwd.innerHTML = "Password correct.";
+                        inputPwd.value = "";
+                        setTimeout(function(){
+                            addClass(statusPwd, "disappear");
+                            removeClass(btnSet, "disappear");
+                            statusSwitch.checked = false;
+                            bgpage.setIsEnabled(false);
+                            browser.storage.local.set({
+                                isEnabled: false,
+                            }, function() {});
+                            chrome.storage.local.remove("unlockPassCode");
+                        }, 500);
+                    }
+                    else{
+                        addClass(inputPwd, "disappear");
+                        removeClass(statusPwd, "disappear");
+                        statusPwd.innerHTML = "Password incorrect.";
+                        inputPwd.value = "";
+                        setTimeout(function(){
+                            removeClass(inputPwd, "disappear");
+                            addClass(statusPwd, "disappear");
+                        }, 500); 
+                    }
+                }
+            });
+        }
+    })
 
     window.onload = init;
 })();
